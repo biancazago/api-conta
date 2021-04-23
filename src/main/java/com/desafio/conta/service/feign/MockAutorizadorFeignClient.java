61c585014f24f6897a4f6d22a93ed7bd46f6aeae
bianca.zago <bianca.zago@basis.com.br>
2021-04-23 01:09:55 -0300

    ajustes no MockAutorizadorFeign

--- src/main/java/com/desafio/conta/service/feign/MockAutorizadorFeignClient.java
+++ src/main/java/com/desafio/conta/service/feign/MockAutorizadorFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@FeignClient(name = "tranferencia", url = "https://run.mocky.io/v3")
+@FeignClient(name = "tranferencia", url = "${autorizador.url}")
 public interface MockAutorizadorFeignClient {
 
 
